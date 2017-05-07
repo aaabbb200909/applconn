7fb8661e5fd8ecdd75be077ba8f3fdd4c6a1c477
Tatsuya Naganawa <tatsuyan201101@gmail.com>
2017-05-07 13:57:56 +0900

    added logic to import haproxy config file

--- batchregister.py
+++ batchregister.py
@@ -4,18 +4,20 @@
 #json_filepath="/usr/local/applconn/applconn.json"
 json_filepath="/var/tmp/applconn/applconn.json"
 elasticsearchurl='localhost:9200'
+#elasticsearchurl='172.17.0.5:9200'
 elasticsearch_path='/applconn/'
+rsyncgitpath='/var/tmp/rsyncgit/'
 ###
 
 import os
 import sys
+import glob
 import json
 import networkx as nx
 from networkx.readwrite import json_graph
 
 def import_rsyncgit(G):
     '''rsyncgit'''
-    rsyncgitpath='/var/lib/git/rsyncbackup.git/'
     l=os.popen('ls '+rsyncgitpath).readlines()
     for st in l:
      tmp=st.strip()
@@ -58,14 +60,11 @@ def import_libvirt(G):
     '''libvirt'''
     pass
 
-
 def import_ansible_facts(G):
     '''import ansible facts
     create-fact command: ansible all -i /tmp/hosts -m setup -t /tmp/ansible_facts
     '''
     ansible_facts_dir='/tmp/ansible_facts/'
-    import glob
-    import os
     for factpath in glob.glob(ansible_facts_dir+'*'):
      nodename=os.path.basename(factpath)
      with open(factpath) as f:
@@ -73,6 +72,41 @@ def import_ansible_facts(G):
      # remove some info which ES don't like
      js["ansible_facts"]["ansible_python"]["version_info"]=[]
      G.add_node(nodename, js)
+
+def import_haproxy(G):
+    '''import haproxy.cfg
+    listen main1081
+        bind *:1081
+        server server1 172.17.0.4:1081
+    '''
+    haproxyfilepaths=glob.glob(rsyncgitpath + "/*/etc/haproxy/haproxy.cfg")
+    for haproxycfgpath in haproxyfilepaths:
+     nodename=haproxycfgpath.split("/")[-4]
+     with open(haproxycfgpath) as f:
+      l=f.readlines()
+     apps=[]
+     for st in l:
+      st = st.rstrip()
+      tmp=st.split()
+      if (st.find("listen") > -1):
+       app={}
+       app["name"]=tmp[1]
+      elif (st.find("bind") > -1):
+       app["bind"]=tmp[1]
+      elif (st.find("  server ") > -1):
+       backend_ip_port=tmp[2]
+       ttmp=backend_ip_port.split(':')
+       #print (ttmp)
+       app["backend_ip"]=ttmp[0]
+       app["backend_port"]=ttmp[1]
+       #
+       apps.append(app)
+       app=None
+     # add to graph
+     G.add_edge(nodename, nodename+"-haproxy")
+     for app in apps:
+      G.add_edge(nodename+"-haproxy", nodename+app["bind"])
+      G.add_edge(nodename+app["bind"], app["backend_ip"])
 
 def import_testlogic(G):
     G.add_node('1')
@@ -91,7 +125,8 @@ def import_testlogic(G):
      G.node[n]['name'] = n
 
 list_import_def=[
-    #import_ansible_facts,
+#    import_ansible_facts,
+#    import_haproxy,
     import_testlogic
 ]
 
