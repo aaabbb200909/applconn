005ea90b97819861a25549e2081693800f65e8ae
Tatsuya Naganawa <tatsuyan201101@gmail.com>
2017-05-13 11:07:12 +0900

    enabled tick

--- applconn.js
+++ applconn.js
@@ -34,7 +34,7 @@ d3.json(jsonpath, function(json) {
       .attr("cy", function(d) { return d.y; })
       .attr("r", 15)
       .style("fill", function(d) { return d.color; })
-      .on("click", function(d) { window.open(d.href); })
+      // .on("click", function(d) { window.open(d.href); })
       .call(force.drag);
 
   node.append("svg:title")
@@ -45,14 +45,15 @@ d3.json(jsonpath, function(json) {
       .duration(1000)
       .style("opacity", 1);
 
-//  force.on("tick", function() {
-//    link.attr("x1", function(d) { return d.source.x; })
-//        .attr("y1", function(d) { return d.source.y; })
-//        .attr("x2", function(d) { return d.target.x; })
-//        .attr("y2", function(d) { return d.target.y; });
-//
-//    node.attr("cx", function(d) { return d.x; })
-//        .attr("cy", function(d) { return d.y; });
-//  });
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  });
+
 });
 
